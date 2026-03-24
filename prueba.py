d69f8867abf021a70cad2be1cab9a2816d6f6e19
José Roberto Landero <josero2602@hotmail.com>
2026-03-23 21:27:47 -0600

    Prueba env

--- prueba.py
+++ prueba.py
@@ -1,1 +1,19 @@
-print("hola mundo")
+import tensorflow as tf
+import torch
+import mlx.core as mx
+
+print("--- Iniciando prueba de frameworks --- \n")
+
+# 1. TENSOR EN TENSORFLOW
+tensor_tf = tf.constant([1.0, 2.0, 3.0])
+print("TensorFlow:\n", tensor_tf, "\n")
+
+# 2. TENSOR EN PYTORCH
+tensor_torch = torch.tensor([1.0, 2.0, 3.0])
+print("PyTorch:\n", tensor_torch, "\n")
+
+# 3. ARRAY EN MLX (El poder de tu Mac)
+array_mlx = mx.array([1.0, 2.0, 3.0])
+print("MLX:\n", array_mlx, "\n")
+
+print("¡Todo funciona a la perfección!")
